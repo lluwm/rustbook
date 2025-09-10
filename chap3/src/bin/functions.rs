3883a043e30b57bd67c1a58b648df8c0b768c741
Lei Lu <llu@email.wm.edu>
2025-09-09 23:56:26 -0700

    chap3.3

---
+++ chap3/src/bin/functions.rs
@@ -0,0 +1,15 @@
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn main() {
+    let x = five();
+    let y = plus_one(5);
+
+    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
+}
